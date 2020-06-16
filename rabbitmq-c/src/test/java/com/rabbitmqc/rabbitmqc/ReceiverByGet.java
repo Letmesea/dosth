4d81111a433e04b1ae368445c272214443e322a3
Letmesea <LIANGZHONGHAI3@ceair.com>
2020-06-16 22:24:48 +0800

    测试消费

--- rabbitmq-c/src/test/java/com/rabbitmqc/rabbitmqc/ReceiverByGet.java
+++ rabbitmq-c/src/test/java/com/rabbitmqc/rabbitmqc/ReceiverByGet.java
@@ -1,10 +1,16 @@
 package com.rabbitmqc.rabbitmqc;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.entity.FlightBody;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.GetResponse;
 
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -19,14 +25,15 @@ public class ReceiverByGet {
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost("192.168.157.123");
         factory.setVirtualHost("p1");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 
         while (true) {
-            GetResponse resp = channel.basicGet(QUEUE_NAME, true);
+            GetResponse resp = channel.basicGet(QUEUE_NAME, false);
+            List<FlightBody> flightBody = (List<FlightBody>) ByteToObject(resp.getBody());
             if (resp == null) {
                 System.out.println("Get Nothing!");
                 TimeUnit.MILLISECONDS.sleep(1000);
@@ -36,5 +43,22 @@ public class ReceiverByGet {
                 TimeUnit.MILLISECONDS.sleep(500);
             }
         }
+    }
+    private static Object ByteToObject(byte[] bytes) {
+        Object obj = null;
+        try {
+            // bytearray to object
+            ByteArrayInputStream bi = new ByteArrayInputStream(bytes);
+            ObjectInputStream oi = new ObjectInputStream(bi);
+
+
+            obj = oi.readObject();
+            bi.close();
+            oi.close();
+        } catch (Exception e) {
+            System.out.println("translation" + e.getMessage());
+            e.printStackTrace();
+        }
+        return obj;
     }
 }
