0e42765385ef55478991c18963c44538e5a9005b
LIANGZHONGHAI3 <LIANGZHONGHAI3@ceair.com>
2020-11-24 17:51:34 +0800

    test

---
+++ rabbitmq-p/src/test/java/com/rabbitmq/rabbitmq/dto/Person.java
@@ -0,0 +1,17 @@
+package com.rabbitmq.rabbitmq.dto;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String mail;
+    private Integer age;
+    private String sex;
+    public Person(String name,String mail,int age,String sex){
+        this.name = name;
+        this.mail = mail;
+        this.age= age;
+        this.sex = sex;
+    }
+}
